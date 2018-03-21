fa2971461913605cf8d758b1baf0f6e31f066bb7
Andrey Samokhvalov <andrew.shvv@gmail.com>
2018-03-22 00:07:29 +0300

    manager, utils: add closed channel check

--- manager/router/emulation/utils.go
+++ manager/router/emulation/utils.go
@@ -1,10 +1,24 @@
 package emulation
 
-import "github.com/go-errors/errors"
+import (
+	"github.com/go-errors/errors"
+)
 
 func fail(errChan chan error, format string, params ...interface{}) error {
 	err := errors.Errorf(format, params)
 	log.Error(err)
-	errChan <- err
+
+	// Ensure that we are not sending in closed channel.
+	select {
+	case <-errChan:
+		return err
+	default:
+	}
+
+	select {
+	case errChan <- err:
+	default:
+	}
+
 	return err
 }
