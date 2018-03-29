3c8755027c4e591d40e07f70823a153d21264133
Aleksandr Zhuravljov <a.s.zhuravljov@gmail.com>
2018-03-29 19:17:25 +0500

    activity generation: transaction stream enhancement

---
+++ optimization/activity/transstream.py
@@ -0,0 +1,38 @@
+import json
+
+import sys
+
+sys.path.append('../')
+
+
+def transstream_gen(file_name_inlet):
+    with open(file_name_inlet) as f:
+        inlet = json.load(f)
+
+    with open(inlet['timematr_file_name']) as f:
+        timematr = json.load(f)['timematr']
+
+    with open(inlet['transmatr_file_name']) as f:
+        transmatr = json.load(f)['transmatr']
+
+    transstream_unsort = []
+    for i in range(len(timematr)):
+        for j in range(len(timematr[i])):
+            if len(timematr[i][j]) > 1:
+                for k in range(1, len(timematr[i][j])):
+                    transstream_unsort.append(
+                        dict(time=timematr[i][j][k], sender_ind=i,
+                             receiver_ind=j, trans=transmatr[i][j][k]))
+
+    def take_time(trans):
+        return trans['time']
+
+    transstream = sorted(transstream_unsort, key=take_time)
+
+    with open(inlet['transstream_file_name'], 'w') as f:
+        json.dump({'transstream': transstream}, f, sort_keys=True,
+                  indent=4 * ' ')
+
+
+if __name__ == '__main__':
+    transstream_gen(file_name_inlet='inlet/transstream_inlet.json')
