29a5e0d068e663e381202dcffeb38f5f9bfe5605
Andrey Samokhvalov <andrew.shvv@gmail.com>
2018-04-09 22:09:28 +0300

    manager, metrics: add init version

---
+++ manager/metrics/crypto/backend.go
@@ -0,0 +1,220 @@
+package crypto
+
+import (
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/go-errors/errors"
+	"github.com/bitlum/hub/manager/metrics"
+)
+
+const (
+	// subsystem is used as the second part in the name of the metric,
+	// after the metrics namespace.
+	subsystem = "crypto"
+
+	// methodLabel is used to distinguish different methods during the
+	// process of metric analysis and alert rule constructing.
+	methodLabel = "method"
+
+	// assetLabel is used to distinguish different currency and daemon on the
+	// metrics server.
+	assetLabel = "asset"
+
+	// severityLabel is used to distinguish different error codes by its
+	// level of importance.
+	severityLabel = "severity"
+)
+
+// MetricsBackend is a system which is responsible for receiving and storing
+// the connector metricsBackend.
+type MetricsBackend interface {
+	AddMethod(asset, method string)
+	AddError(asset, method string, severity metrics.Severity)
+	AddPanic(asset, method string)
+	AddMethodDuration(asset, method string, dur time.Duration)
+}
+
+// EmptyBackend is used as an empty metricsBackend backend in order to avoid
+type EmptyBackend struct{}
+
+func (b *EmptyBackend) AddMethod(query string)                            {}
+func (b *EmptyBackend) AddError(query string, errCode string)             {}
+func (b *EmptyBackend) AddPanic(query string)                             {}
+func (b *EmptyBackend) AddMethodDuration(query string, dur time.Duration) {}
+
+// PrometheusBackend is the main subsystem metrics implementation. Uses
+// prometheus metrics singletons defined above.
+//
+// WARN: Method name should be taken from limited set.
+// Don't use dynamic naming, it may cause dramatic increase of the amount of
+// data on the metric server.
+//
+// NOTE: Non-pointer receiver made by intent to avoid conflict in the system
+// with parallel metrics report.
+type PrometheusBackend struct {
+	methodsTotal          *prometheus.CounterVec
+	errorsTotal           *prometheus.CounterVec
+	panicsTotal           *prometheus.CounterVec
+	methodDurationSeconds *prometheus.HistogramVec
+}
+
+// AddMethod increases method counter for the given method name.
+//
+// NOTE: Non-pointer receiver made by intent to avoid conflict in the system
+// with parallel metrics report.
+func (m PrometheusBackend) AddMethod(asset, method string) {
+	m.methodsTotal.With(
+		prometheus.Labels{
+			methodLabel: method,
+			assetLabel:  asset,
+		},
+	).Add(1)
+}
+
+// AddError increases error counter for the given method name.
+//
+// WARN: Error code name should be taken from limited set.
+// Don't use dynamic naming, it may cause dramatic increase of the amount of
+// data on the metric server.
+//
+// NOTE: Non-pointer receiver made by intent to avoid conflict in the system
+// with parallel metrics report.
+func (m PrometheusBackend) AddError(asset, method string,
+	severity metrics.Severity) {
+	m.errorsTotal.With(
+		prometheus.Labels{
+			methodLabel:   method,
+			severityLabel: string(severity),
+			assetLabel:    asset,
+		},
+	).Add(1)
+}
+
+// AddPanic increases panic counter for the given method name.
+//
+// NOTE: Non-pointer receiver made by intent to avoid conflict in the system
+// with parallel metrics report.
+func (m PrometheusBackend) AddPanic(asset, method string) {
+	m.panicsTotal.With(
+		prometheus.Labels{
+			methodLabel: method,
+			assetLabel:  asset,
+		},
+	).Add(1)
+}
+
+// AddMethodDuration sends the metric with how much time method has taken
+// to proceed.
+//
+// NOTE: Non-pointer receiver made by intent to avoid conflict in the system
+// with parallel metrics report.
+func (m PrometheusBackend) AddMethodDuration(asset, method string,
+	dur time.Duration) {
+	m.methodDurationSeconds.With(
+		prometheus.Labels{
+			methodLabel: method,
+			assetLabel:  asset,
+		},
+	).Observe(dur.Seconds())
+}
+
+// InitMetricsBackend creates subsystem metrics for specified
+// net. Creates and tries to register metrics singletons. If register was
+// already done, than function not returning error.
+func InitMetricsBackend(net string) (MetricsBackend, error) {
+	backend := PrometheusBackend{}
+
+	backend.methodsTotal = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Namespace: metrics.Namespace,
+			Subsystem: subsystem,
+			Name:      "methods_total",
+			Help:      "Total methods processed",
+			ConstLabels: prometheus.Labels{
+				metrics.NetLabel: net,
+			},
+		},
+		[]string{
+			methodLabel,
+			assetLabel,
+		},
+	)
+
+	if err := prometheus.Register(backend.methodsTotal); err != nil {
+		return backend, errors.Errorf(
+			"unable to register 'methodsTotal' metric:" +
+				err.Error())
+
+	}
+
+	backend.errorsTotal = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Namespace: metrics.Namespace,
+			Subsystem: subsystem,
+			Name:      "errors_total",
+			Help:      "Total methods which processing ended with error",
+			ConstLabels: prometheus.Labels{
+				metrics.NetLabel: net,
+			},
+		},
+		[]string{
+			methodLabel,
+			assetLabel,
+			severityLabel,
+		},
+	)
+
+	if err := prometheus.Register(backend.errorsTotal); err != nil {
+		return backend, errors.Errorf(
+			"unable to register 'errorsTotal' metric: " +
+				err.Error())
+
+	}
+
+	backend.panicsTotal = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Namespace: metrics.Namespace,
+			Subsystem: subsystem,
+			Name:      "panics_total",
+			Help:      "Total methods which processing ended with panic",
+			ConstLabels: prometheus.Labels{
+				metrics.NetLabel: net,
+			},
+		},
+		[]string{
+			methodLabel,
+			assetLabel,
+		},
+	)
+
+	if err := prometheus.Register(backend.panicsTotal); err != nil {
+		return backend, errors.Errorf(
+			"unable to register 'panicsTotal' metric: " +
+				err.Error())
+	}
+
+	backend.methodDurationSeconds = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Namespace: metrics.Namespace,
+			Subsystem: subsystem,
+			Name:      "method_duration_seconds",
+			Help:      "Method processing duration in seconds",
+			ConstLabels: prometheus.Labels{
+				metrics.NetLabel: net,
+			},
+		},
+		[]string{
+			methodLabel,
+			assetLabel,
+		},
+	)
+
+	if err := prometheus.Register(backend.methodDurationSeconds); err != nil {
+		return backend, errors.Errorf(
+			"unable to register 'methodDurationSeconds' metric: " +
+				err.Error())
+	}
+
+	return backend, nil
+}
---
+++ manager/metrics/crypto/metric.go
@@ -0,0 +1,99 @@
+package crypto
+
+import (
+	"time"
+	"runtime/debug"
+	"strings"
+	"github.com/bitlum/hub/manager/metrics"
+)
+
+// Metric is an enhancement of Metric backend, which is more suited for this
+// package usage.
+type Metric struct {
+	// backend is an entity which is responsible for collecting and storing
+	// the metricsBackend.
+	backend MetricsBackend
+
+	// methodName is name used as value for label when adding metricsBackend.
+	// Supposed to be set by `NewMetric` method.
+	methodName string
+
+	// startTime is a method start time used to measure method
+	// duration in `AddMethodDuration` method. Supposed to be set
+	// in `NewMetric` method to now.
+	startTime time.Time
+
+	// asset is an asset with which cryptocurrency daemon is working.
+	// Used as an additional label in the Metric server.
+	asset string
+}
+
+// NewMetric creates new Metric.
+//
+// Note: we use not pointer type receiver so any changes within method
+// do not change original struct fields. Each call creates new `metricsBackend`
+// with copied fields.
+func NewMetric(asset, method string, backend MetricsBackend) Metric {
+	m := Metric{}
+
+	m.backend = backend
+	m.methodName = method
+	m.startTime = time.Now()
+	m.asset = asset
+
+	m.backend.AddMethod(m.asset, method)
+	return m
+}
+
+// AddError adds error metric with specified error.
+//
+// Note: we use not pointer type receiver so any changes within method
+// do not change original struct fields. Each call creates new `metricsBackend`
+// with copied fields.
+func (m Metric) AddError(severity metrics.Severity) {
+	m.backend.AddError(m.asset, m.methodName, severity)
+}
+
+// AddPanic adds panic metric
+func (m Metric) AddPanic() {
+	m.backend.AddPanic(m.asset, m.methodName)
+}
+
+// AddMethodDuration adds method duration metric. Supposed to be
+// called after `NewMetric` which defines `startTime`. Calculates
+// duration using `startTime` and now as end time.
+//
+// Note: we use not pointer type receiver so any changes within method
+// do not change original struct fields. Each call creates new `metricsBackend`
+// with copied fields.
+func (m Metric) AddMethodDuration() {
+	if m.startTime.Equal(time.Time{}) {
+		panic("not initialised method")
+	}
+
+	dur := time.Now().Sub(m.startTime)
+	m.backend.AddMethodDuration(m.asset, m.methodName, dur)
+}
+
+// Finish used as defer in handlers, to ensure that we track panics and
+// measure handler time.
+func (m Metric) Finish() {
+	m.AddMethodDuration()
+
+	if r := recover(); r != nil {
+		m.AddPanic()
+		panic(stackTrace())
+	}
+}
+
+func stackTrace() string {
+	s := string(debug.Stack())
+	ls := strings.Split(s, "\n")
+	for i, l := range ls {
+		if strings.Index(l, "src/runtime/panic.go") != -1 && i > 0 &&
+			strings.Index(ls[i-1], "panic(") == 0 {
+			return strings.TrimSpace(strings.Join(ls[i+2:], "\n"))
+		}
+	}
+	return s
+}
