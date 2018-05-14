0991441f773e855de83f162907fc8420aacb8ada
Andrey Samokhvalov <andrew.shvv@gmail.com>
2018-05-14 13:06:06 +0300

    manger, router, lnd: rename db

--- manager/router/lnd/db.go
+++ manager/router/lnd/db.go
@@ -1,6 +1,8 @@
 package lnd
 
-type DB interface {
+// SyncStorage is the storage which is needed for keeping the info about hub
+// synchronisation state.
+type SyncStorage interface {
 	// PutLastForwardingIndex is used to save last forward pagination index
 	// which was used for getting forwarding events. With this we avoid
 	// processing of the same forwarding events twice.
@@ -19,28 +21,28 @@ type DB interface {
 	ChannelsState() (map[string]string, error)
 }
 
-type InMemory struct {
+type InMemorySyncStorage struct {
 	lastIndex     uint32
 	channelsState map[string]string
 }
 
-func (db *InMemory) StartUpdate() {}
-func (db *InMemory) Flush()       {}
+func (db *InMemorySyncStorage) StartUpdate() {}
+func (db *InMemorySyncStorage) Flush()       {}
 
-func (db *InMemory) PutLastForwardingIndex(lastIndex uint32) error {
+func (db *InMemorySyncStorage) PutLastForwardingIndex(lastIndex uint32) error {
 	db.lastIndex = lastIndex
 	return nil
 }
 
-func (db *InMemory) LastForwardingIndex() (uint32, error) {
+func (db *InMemorySyncStorage) LastForwardingIndex() (uint32, error) {
 	return db.lastIndex, nil
 }
 
-func (db *InMemory) ChannelsState() (map[string]string, error) {
+func (db *InMemorySyncStorage) ChannelsState() (map[string]string, error) {
 	return db.channelsState, nil
 }
 
-func (db *InMemory) PutChannelsState(channelsState map[string]string) error {
+func (db *InMemorySyncStorage) PutChannelsState(channelsState map[string]string) error {
 	db.channelsState = channelsState
 	return nil
 }
