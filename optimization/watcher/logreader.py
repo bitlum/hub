78e5b61b255a4fa8bc8e22f84d2131af761b0129
Aleksandr Zhuravljov <a.s.zhuravljov@gmail.com>
2018-05-17 20:59:46 +0500

    optimization, watcher: added earned to the class LogReader

--- optimization/watcher/logreader.py
+++ optimization/watcher/logreader.py
@@ -39,7 +39,7 @@ class LogReader:
 
                 self.convert()
 
-                # print(self.smart_log)
+                print(self.smart_log)
 
     def read_message(self):
         log = proto.Log()
