bd8840cb66067f4702abc1a1cc5ae095521e422e
Andrey Samokhvalov <andrew.shvv@gmail.com>
2018-06-19 17:50:22 +0300

    manager, db: remove boltdb

--- manager/db/boltdb/errors.go
+++
@@ -1,9 +0,0 @@
-package boltdb
-
-import "fmt"
-
-var (
-	// ErrMetaNotFound is returned when meta bucket hasn't been
-	// created.
-	ErrMetaNotFound = fmt.Errorf("unable to locate meta information")
-)
