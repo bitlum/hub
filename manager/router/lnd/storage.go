8b8c8811cec1df5565ec3bb99b1ffcf4ac627514
Andrey Samokhvalov <andrew.shvv@gmail.com>
2018-06-24 19:57:33 +0300

    manager, router, lnd: use new storages

--- manager/router/lnd/storage.go
+++ manager/router/lnd/storage.go
@@ -2,11 +2,17 @@ package lnd
 
 import "github.com/bitlum/hub/manager/router"
 
-// SyncStorage is the storage which is needed for keeping the info about hub
+// RouterStorage is the storage which is needed for keeping the info about hub
 // synchronisation state.
-type SyncStorage interface {
+type RouterStorage interface {
 	router.ChannelStorage
+	router.UserStorage
+	router.PaymentStorage
+	router.InfoStorage
+	SyncStorage
+}
 
+type SyncStorage interface {
 	// PutLastForwardingIndex is used to save last forward pagination index
 	// which was used for getting forwarding events. With this we avoid
 	// processing of the same forwarding events twice.
@@ -15,17 +21,4 @@ type SyncStorage interface {
 	// LastForwardingIndex return last lnd forwarding pagination index of
 	// which were preceded by the hub.
 	LastForwardingIndex() (uint32, error)
-}
-
-type InMemorySyncStorage struct {
-	lastIndex uint32
-}
-
-func (db *InMemorySyncStorage) PutLastForwardingIndex(lastIndex uint32) error {
-	db.lastIndex = lastIndex
-	return nil
-}
-
-func (db *InMemorySyncStorage) LastForwardingIndex() (uint32, error) {
-	return db.lastIndex, nil
 }
