6edd9bb17cf73ea45f3971cc14e5507ca158bd42
Andrey Samokhvalov <andrew.shvv@gmail.com>
2018-06-24 20:04:07 +0300

    manager, router: divide objects storage in order to simplify testing in future

--- manager/router/storage.go
+++ manager/router/storage.go
@@ -1,114 +1,46 @@
 package router
 
-// InfoStorage represents storage of the hub public information,
-// and is used as cached between subsystem fetching the info and retrieving
-// the info.
+// ChannelStorage contains all necessary hooks to store channel object.
+type ChannelStorage interface {
+	// AddChannelState adds state to the channel's state array. State array
+	// should be initialised in the Channel object on the stage of getting
+	// channels.
+	AddChannelState(chanID ChannelID, state *ChannelState) error
+
+	// UpdateChannel saves or update channel without saving its states.
+	UpdateChannel(channel *Channel) error
+
+	// Channels returns all channels and its states.
+	Channels() ([]*Channel, error)
+
+	// RemoveChannel removes the channel.
+	RemoveChannel(channel *Channel) error
+}
+
+// UserStorage contains all necessary hooks to store user object.
+type UserStorage interface {
+	// UpdateUser creates or updates user in database.
+	UpdateUser(user *User) error
+
+	// Users returns all users who are somehow related or were realted to the
+	// hub.
+	Users() ([]*User, error)
+}
+
+// InfoStorage contains all necessary hooks to store hub info objects.
 type InfoStorage interface {
-	// StorePayment saves the payment.
-	StorePayment(payment *DbPayment) error
-
-	// UpdatePeers updates information about set of online and peers
-	// connected to the hub.
-	UpdatePeers(peers []*DbPeer) error
-
 	// UpdateInfo updates information about the hub lightning network node.
-	UpdateInfo(info *DbInfo) error
-
-	// Payments returns the payments happening inside the hub local network,
-	Payments() ([]*DbPayment, error)
-
-	// Peers return the peers active and connected to the hub.
-	Peers() ([]*DbPeer, error)
+	UpdateInfo(info *Info) error
 
 	// Info returns hub lighting network node information.
-	Info() (*DbInfo, error)
+	Info() (*Info, error)
 }
 
-type DbNodeInfo struct {
-	Alias          string
-	Host           string
-	Port           string
-	IdentityPubKey string
-}
+// PaymentStorage contains all necessary hooks to store payment object.
+type PaymentStorage interface {
+	// Payments returns the payments happening inside the hub local network,
+	Payments() ([]*Payment, error)
 
-type DbNeutrinoInfo struct {
-	Host string
-	Port string
-}
-
-type DbInfo struct {
-	Version      string
-	Network      string
-	BlockHeight  uint32
-	BlockHash    string
-	NodeInfo     *DbNodeInfo
-	NeutrinoInfo *DbNeutrinoInfo
-}
-
-type DbPeer struct {
-	PubKey       string
-	Alias        string
-	LockedByPeer int64
-	LockedByHub  int64
-}
-
-type DbPayment struct {
-	FromPeer string
-	ToPeer   string
-	Amount   int64
-	Status   string
-	Type     string
-	Time     int64
-}
-
-func NewInMemoryHubStorage() *InMemoryInfoStorage {
-	return &InMemoryInfoStorage{}
-}
-
-// InMemoryInfoStorage represent in-memory storage for hub public information.
-type InMemoryInfoStorage struct {
-	info     *DbInfo
-	payments []*DbPayment
-	peers    []*DbPeer
-}
-
-func (s *InMemoryInfoStorage) StorePayment(payment *DbPayment) error {
-	s.payments = append(s.payments, payment)
-	return nil
-}
-
-func (s *InMemoryInfoStorage) UpdatePeers(peers []*DbPeer) error {
-	s.peers = peers
-	return nil
-}
-
-func (s *InMemoryInfoStorage) UpdateInfo(info *DbInfo) error {
-	s.info = info
-	return nil
-}
-
-func (s *InMemoryInfoStorage) Payments() ([]*DbPayment, error) {
-	if s.payments == nil {
-		return []*DbPayment{}, nil
-	}
-
-	return s.payments, nil
-}
-func (s *InMemoryInfoStorage) Peers() ([]*DbPeer, error) {
-	if s.peers == nil {
-		return []*DbPeer{}, nil
-	}
-
-	return s.peers, nil
-}
-
-func (s *InMemoryInfoStorage) Info() (*DbInfo, error) {
-	if s.info == nil {
-		return &DbInfo{
-			NodeInfo:     &DbNodeInfo{},
-			NeutrinoInfo: &DbNeutrinoInfo{},
-		}, nil
-	}
-
-	return s.info, nil
+	// StorePayment saves the payment.
+	StorePayment(payment *Payment) error
 }
